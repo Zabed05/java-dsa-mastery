7a76fe1c39c2e083d9c03b0e4988cdba49c80990
Zabed05 <zabedkhan7869@gmail.com>
2026-09-12 21:13:49 +0530

    Diameter of Binary Tree

--- 12_Trees/Questions/BT.java
+++ 12_Trees/Questions/BT.java
@@ -320,4 +320,24 @@ public class BT {
         }
         return true;
     }
+
+    //--------------------DFS--------------------//
+    // Diameter of Binary Tree (LC: 543)
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return  diameter;
+    }
+    private int height(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int dia = leftHeight + rightHeight + 1;
+        diameter = Math.max(diameter, dia);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
